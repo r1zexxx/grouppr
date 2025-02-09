4b02934fe4f3c1dc0f3fe76ad9fa61c1b05d4554
dragonbreath031 <132835837+dragonbreath031@users.noreply.github.com>
2025-02-09 21:01:27 +0300

    Add files via upload

--- booking_system/users/views.py
+++ booking_system/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Booking
+
 
 # Create your views here.
+def main(request):
+    book = Booking.objects.all()
+    return render(request, "main.html", {"bookings": book})
+
+def book_detail_view(reguest, book_id):
+    book = Booking.objects.get(id=book_id)
+    return render(reguest, "booking.html", {"booking": [book]})
