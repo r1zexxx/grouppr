64695071b946e43f853fd884854dfd98db79b25b
dragonbreath031 <132835837+dragonbreath031@users.noreply.github.com>
2025-02-16 17:29:51 +0300

    Add files via upload

--- booking_system/users/models.py
+++ booking_system/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Booking(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    creater = models.ForeignKey(User, related_name="books", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.author})"
+    
+class Rooms(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
