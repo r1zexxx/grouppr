2e8e07d9d99452e4c538b0c18c6883346da4cee5
dragonbreath031 <132835837+dragonbreath031@users.noreply.github.com>
2025-02-16 20:33:34 +0300

    Update views.py

--- booking_system/users/views.py
+++ booking_system/users/views.py
@@ -7,10 +7,10 @@ def main(request):
     bookin = Booking.objects.all()
     return render(request, "main.html", {"bookings": bookin})
 
-def bookin_detail_view(reguest, bookin_id):
+def bookin(reguest, bookin_id):
     bookin = Booking.objects.get(id=bookin_id)
     return render(reguest, "booking.html", {"booking": [bookin]})
 
-def rooms_detail_view(reguest, rooms_id):
+def rooms(reguest, rooms_id):
     rooms = Rooms.objects.get(id=rooms_id)
     return render(reguest, "rooms.html", {"rooms": [rooms]})
